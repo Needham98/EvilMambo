6fe1d36554601587c40f89e8f5c1330cbadacdf7
hell2o <jes566@york.ac.uk>
2017-11-13 12:24:22 +0000

    Added grid system

---
+++ Evil geese/Assets/Grid.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid {
+	Dictionary<ulong, GridPosition> gridDictionary;
+	public Grid(){
+		this.gridDictionary = new Dictionary<ulong, GridPosition> ();
+	}
+
+	public GridPosition getPosition(int x, int y){
+		try{
+			return gridDictionary[GridPosition.hashablePosition(x,y)];
+		} catch (KeyNotFoundException){
+			return null;
+		}
+	}
+
+	public void setPosition( GridPosition pos){
+		gridDictionary.Add (pos.hashable (), pos);
+	}
+
+	public bool clearPosition(int x, int y){
+		return gridDictionary.Remove (GridPosition.hashablePosition (x, y));
+	}
+
+	public bool clearPosition(GridPosition pos){
+		return gridDictionary.Remove (pos.hashable ());
+	}
+}
+
+public class GridPosition {
+	public int x;
+	public int y;
+	public GameObject interactionObject;
+	public GameObject stepOn;	
+
+	public GridPosition(int x, int y, GameObject interactionObject = null, GameObject stepOn = null){
+		this.x = x;
+		this.y = y;
+		this.interactionObject = interactionObject;
+		this.stepOn = stepOn;
+	}
+	public static ulong hashablePosition (int x, int y){ 
+		ulong hashable = (ulong)(uint) x;
+		hashable += ((ulong)(uint) y) << 32;
+		return hashable;
+	}
+
+	public ulong hashable(){
+		return hashablePosition (this.x, this.y);
+	}
+}
---
+++ Evil geese/Assets/Unit tests/PositionCollisionTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionCollisionTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int	count = 0;
+		Dictionary<ulong, int> dict = new Dictionary<ulong,int> ();
+		for (int x = -256; x < 256; x += 1) {
+			for (int y = -256; y < 256; y += 1) {
+				count += 1;
+				ulong value = GridPosition.hashablePosition (x, y);
+				if (dict.ContainsKey (value)) {
+					Debug.Log (value);
+				}
+				dict.Add (value, 1);
+			}
+		}
+		if (dict.Count == count) {
+			Debug.Log ("Passed position collision test");
+		} else {
+			Debug.Log ("FAILURE: position collision test!");
+			Debug.Log (dict.Count);
+		}
+	}
+}
