2d63426f5450ff56f00766bfb03f25c6960c01e7
hell2o <jes566@york.ac.uk>
2017-11-13 17:23:34 +0000

    added the ability to search for a grid position at a specific point, and modify it in editor

--- Evil geese/Assets/GridManager.cs
+++ Evil geese/Assets/GridManager.cs
@@ -5,13 +5,15 @@ using UnityEditor;
 
 
 [CustomEditor(typeof(Grid))]
+[CanEditMultipleObjects]
 public class GridManager : Editor {
 	int tempx = 0;
 	int tempy = 0;
+	GridPosition pos;
 	public override void OnInspectorGUI(){
 		Grid myTarget = (Grid)target;
 		myTarget.OnAfterDeserialize ();
-		DrawDefaultInspector ();
+		//DrawDefaultInspector ();
 
 		EditorGUILayout.BeginHorizontal (null);
 		EditorGUILayout.PrefixLabel(new GUIContent("x:"));
@@ -28,6 +30,36 @@ public class GridManager : Editor {
 		}
 		if (GUILayout.Button ("remove")) {
 			myTarget.clearPosition (tempx, tempy);
+		}
+		if (GUILayout.Button ("search")) {
+			pos = myTarget.getPosition (tempx, tempy);
+		}
+		if (pos != null) {
+			EditorGUILayout.BeginHorizontal (null);
+			EditorGUILayout.PrefixLabel(new GUIContent("x:"));
+			pos.x = EditorGUILayout.IntField (pos.x, null);
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.BeginHorizontal (null);
+			EditorGUILayout.PrefixLabel(new GUIContent("y:"));
+			pos.y = EditorGUILayout.IntField (pos.y, null);
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.BeginHorizontal (null);
+			EditorGUILayout.PrefixLabel(new GUIContent("blocked:"));
+			pos.blocked = EditorGUILayout.Toggle (pos.blocked, null);
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.BeginHorizontal (null);
+			EditorGUILayout.PrefixLabel(new GUIContent("interaction:"));
+			pos.interactionObject = (GameObject) EditorGUILayout.ObjectField (pos.interactionObject, null, allowSceneObjects: true);
+			EditorGUILayout.EndHorizontal ();
+
+			EditorGUILayout.BeginHorizontal (null);
+			EditorGUILayout.PrefixLabel(new GUIContent("step on:"));
+			pos.stepOn = (GameObject) EditorGUILayout.ObjectField (pos.stepOn, null, allowSceneObjects: true);
+			EditorGUILayout.EndHorizontal ();
+
 		}
 		myTarget.OnBeforeSerialize ();
 	}
