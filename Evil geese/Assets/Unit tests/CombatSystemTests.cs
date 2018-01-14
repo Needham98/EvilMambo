b6c3317da0b2ee78bd982010b3ecb90bf2accd65
hell2o <jes566@york.ac.uk>
2018-01-14 15:33:59 +0000

    added some simple unit tests for the combat system and the grid system

---
+++ Evil geese/Assets/Unit tests/CombatSystemTests.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CombatSystemTests {
+
+	[Test]
+	public void CombatSystemTestsCharacterDamage() {
+		CombatCharacter c1 = new CombatCharacter (100, 100, 100, 100, new SimpleAttack (20, 20, "melee"));
+		CombatCharacter c2 = new CombatCharacter (100, 100, 100, 100, new SimpleAttack (20, 20, "melee"));
+
+		Assert.AreEqual (100, c1.health);
+		List<CombatCharacter> l = new List<CombatCharacter> ();
+		l.Add (c1);
+		c2.basicAttack.doAbility(l, c2);
+		Assert.AreEqual (80, c1.health);
+
+	}
+}
---
+++ Evil geese/Assets/Unit tests/GridSystemTests.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class GridSystemTests {
+
+	[Test]
+	public void GridSystemTestsCollisions() {
+		GameObject o = new GameObject ();
+		Grid g = o.AddComponent<Grid> ();
+		Assert.IsNull (g.getPosition (0, 0));
+		Assert.IsNull (g.getPosition (1, 1));
+		g.setPosition (new GridPosition (0, 0,blocked:true));
+
+		Assert.IsNotNull (g.getPosition (0, 0));
+		Assert.IsNull (g.getPosition (1, 1));
+
+		g.clearPosition (0, 0);
+		Assert.IsNull(g.getPosition(0,0));
+	}
+
+}
